3246867c5b3fadfddb53b481b7fbed86a96a95b4
matos-nelson <nmatos6@gatech.edu>
2023-08-12 18:46:33 -0500

    Created owner dto.

---
+++ src/main/java/org/rent/circle/owner/api/dto/OwnerDto.java
@@ -0,0 +1,23 @@
+package org.rent.circle.owner.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.rent.circle.owner.api.enums.Suffix;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OwnerDto {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private Suffix suffix;
+    private String email;
+    private String phone;
+}
---
+++ src/test/java/org/rent/circle/owner/api/dto/OwnerDtoTest.java
@@ -0,0 +1,20 @@
+package org.rent.circle.owner.api.dto;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.force66.beantester.BeanTester;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class OwnerDtoTest {
+
+    @Test
+    public void OwnerDto_SettersAndGetters_ShouldWork() {
+        // Arrange
+        BeanTester beanTester = new BeanTester();
+
+        // Act
+        beanTester.testBean(OwnerDto.class);
+
+        // Assert
+    }
+}
